c668a3c66a05dbd0b39f9b04fe0518b07d1b0fb8
rav23 <rav12@naver.com>
2020-09-26 10:34:06 +0900

    Add files via upload

--- muno.js
+++ muno.js
@@ -1,1 +1,33 @@
+///////////////////////////////
+// (c) Namopaper Namosoft   //
+// muno.js                 //
+// made by rav23 & muno23 //
+///////////////////////////
+
+// Ver 0.1 Beta
+// Apache License
+
+//겍체 선언
+var munojs = {}
+
+//muno.js 
+
+var vername = "0.1 beta"
+console.log('%c%s','color: Orange; font-size: 50px;','Muno.js')
+console.log(vername)
+
+//Modules
+
+// 문서 Write
+munojs.docwrite = function(docwritecount){ //
+    document.write(docwritecount)  
+}
+
+// Console
+munojs.consolemsg = function(consolemsgcount){
+    console.log(consolemsgcount)
+}
+
+
+
 
